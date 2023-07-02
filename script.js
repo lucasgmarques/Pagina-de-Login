d50410abc6e9dd8ed05cbf09488491bad6649c7a
lucasgmarques <lucasg3.gdh@gmail.com>
2023-07-02 16:40:15 +0000

    Adicionado lógica para enviar os dados para o backend e realizar a autenticação

--- script.js
+++ script.js
@@ -11,6 +11,40 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 
   // Aqui você pode adicionar a lógica para enviar os dados para o backend e realizar a autenticação
 
+  // Criar um objeto com os dados do formulário
+  var formData = {
+    email: username,
+    password: password
+  };
+
+  // Enviar os dados para o servidor usando a Fetch API
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(function(response) {
+    if (response.ok) {
+      // Sucesso - exibir a resposta do servidor
+      return response.text();
+    } else {
+      // Erro - lançar uma exceção para ser capturada no bloco catch
+      throw new Error("Ocorreu um erro durante a autenticação. Por favor, tente novamente.");
+    }
+  })
+  .then(function(data) {
+    // Exibir a resposta do servidor
+    alert(data);
+    // Redirecionar para uma nova página, se necessário
+    // window.location.href = "/dashboard";
+  })
+  .catch(function(error) {
+    // Capturar e exibir o erro
+    alert(error.message);
+  });
+});
 
   // Exemplo de envio dos dados para o backend usando o Fetch API
   function openRegisterPage() {
